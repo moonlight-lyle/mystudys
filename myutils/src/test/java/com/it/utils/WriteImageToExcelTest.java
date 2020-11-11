136aa9ec9adc8d12edb89470e8d4d547f1d8949b
lyle_liu <15755589769@163.com>
2020-11-11 21:45:41 +0800

    将图片插入Excel

---
+++ myutils/src/test/java/com/it/utils/WriteImageToExcelTest.java
@@ -0,0 +1,19 @@
+package com.it.utils;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * 测试将图片插入excel
+ *
+ * @author Lyle
+ * @date 2020/11/11
+ */
+public class WriteImageToExcelTest{
+
+    @Test
+    public void fun(){
+        WriteImageToExcel.writeImageToExcel();
+    }
+
+}
